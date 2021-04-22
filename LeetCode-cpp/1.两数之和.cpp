d9f5718fcc283be78d4d31b119d46d5e6c28b922
Apocaly-pse <apocaly_pse@outlook.com>
2021-04-23 00:03:00 +0800

    11

--- LeetCode-cpp/1.两数之和.cpp
+++ LeetCode-cpp/1.两数之和.cpp
@@ -8,15 +8,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i,j;
-        for (i=0;i<nums.size()-1;i++) {
-            for (j=i+1;j<nums.size();j++) {
-                if (nums[i] +nums[j] ==target) {
-                    return {i,j};
-                }
-            }
-        }
-        return {i,j};
+        // // 方法一: 暴力穷举法
+        // int i,j;
+        // for (i=0;i<nums.size()-1;i++) {
+        //     for (j=i+1;j<nums.size();j++) {
+        //         if (nums[i] +nums[j] ==target) {
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return {i,j};
     }
 };
 // @lc code=end
---
+++ cpp_grammar/pointer/const_Pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+const修饰指针: 常量指针  -> 指针指向的值不可以改,指针指向可以改
+const修饰常量: 指针常量 -> 指针指向不可以改, 指针指向的值可以改
+const指针和常量均修饰  -> 均不能改
+*/
+int main(int argc, char const *argv[])
+{
+    int a=10;
+    int b=10;
+    // // 这个是常量指针
+    // const int *p=&a;
+    // // *p=100;  -> 值不可以改
+    // p = &b;  // 指向可以改
+
+    // // 这个是指针常量
+    // int * const p = &a;
+    // // p = &b;  // 指向不可以改
+    // *p = 20; // ->值可以改
+
+    // 这个是const两者都修饰
+    // const int * const p = &a;
+    // p=&b;
+    // *p = 20;
+    cout<<*p<<endl;
+    cout<<endl;
+    return 0;
+}

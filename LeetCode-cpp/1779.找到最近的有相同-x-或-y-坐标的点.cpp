3f1799c8e43091e26f349e6e44760e1eef11a915
Apocaly-pse <apocaly_pse@outlook.com>
2021-05-05 21:31:57 +0800

    66

---
+++ LeetCode-cpp/1779.找到最近的有相同-x-或-y-坐标的点.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1779 lang=cpp
+ *
+ * [1779] 找到最近的有相同 X 或 Y 坐标的点
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+
+    }
+};
+// @lc code=end
+
